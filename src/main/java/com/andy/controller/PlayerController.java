6b9578d5bb33dcb92db669ade3b7e9e9a5c7db75
greedlend <andy010833@gmail.com>
2021-03-10 19:36:09 +0800

    adds PlayerController

---
+++ src/main/java/com/andy/controller/PlayerController.java
@@ -0,0 +1,21 @@
+package com.andy.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
+
+@RestController
+@RequestMapping(value = "/player")
+public class PlayerController {
+
+    private static Logger logger = LoggerFactory.getLogger(PlayerController.class);
+
+    @RequestMapping(value= "/plusInSpot")
+    public String gateWay() {
+        logger.info("adds one in spot");
+        return "";
+    }
+}
