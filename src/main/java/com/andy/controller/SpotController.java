6633932b2a132c829887db9e96cdd03911fc56ae
Andy.Lin <andy.lin@unicornfintech.com>
2021-03-23 17:04:14 +0800

    adds jedis, avoid repeats submits

--- src/main/java/com/andy/controller/SpotController.java
+++ src/main/java/com/andy/controller/SpotController.java
@@ -90,7 +90,7 @@ public class SpotController {
 
         //todo: adds redis to avoid repeating submit
         HttpSession session = httpServletRequest.getSession();
-        session.getAttribute("test");
+        session.getAttribute("test redis");
         String redisPunchKey = punchKey + uuid;
         Jedis jedis = new Jedis();
         if(null == jedis.get(redisPunchKey)) {
