b6588479d3e4ba3ce7fd7868269611761c1d0e0c
Andy.Lin <andy.lin@unicornfintech.com>
2021-03-25 17:23:41 +0800

    加入Application.java

--- src/main/java/com/andy/Application.java
+++ src/main/java/com/andy/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import redis.clients.jedis.Jedis;
 
+import java.util.UUID;
+
 /**
  * @Author: Lim, Andy
  * @Date: 2020/10/26
@@ -17,6 +19,10 @@ public class Application {
         Class clazz = intArray.getClass();
         System.out.println(clazz.getName()); // [I
         Jedis jedis = new Jedis();
+//
+//        UUID uuid = UUID.randomUUID();
+//        uuid.toString();
+//        UUID uuid = UUID.fromString("5ew1ec5-qweq-qwe0-2134545fs");
 //        jedis.hset
 //        jedis.setnx();
 //        jedis.unlink("key");
