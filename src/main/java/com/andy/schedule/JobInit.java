d82fb684e4f6120c56d6ccc6cd46abfb3f07341d
greedlend <andy010833@gmail.com>
2021-06-06 23:10:25 +0800

    multiple schedule-jobs implemented

--- src/main/java/com/andy/schedule/JobInit.java
+++ src/main/java/com/andy/schedule/JobInit.java
@@ -1,6 +1,7 @@
 package com.andy.schedule;
 
 import com.andy.schedule.job.FirstJob;
+import com.andy.schedule.job.SecondJob;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -17,20 +18,36 @@ public class JobInit implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        JobDetail jobDetail = JobBuilder.newJob(FirstJob.class)
+        JobDetail jobDetail1 = JobBuilder.newJob(FirstJob.class)
                 .withIdentity(ID + " 01")
                 .storeDurably()
                 .build();
-        CronScheduleBuilder scheduleBuilder =
+        JobDetail jobDetail2 = JobBuilder.newJob(SecondJob.class)
+                .withIdentity(ID + " 02")
+                .storeDurably()
+                .build();
+
+        CronScheduleBuilder jobOneScheduleBuilder =
                 CronScheduleBuilder.cronSchedule("0/5 * * * * ? *");
+        CronScheduleBuilder jobTwoScheduleBuilder =
+                CronScheduleBuilder.cronSchedule("0/10 * * * * ? *");
+
         // 建立任務觸發器
-        Trigger trigger = TriggerBuilder.newTrigger()
-                .forJob(jobDetail)
+        Trigger trigger1 = TriggerBuilder.newTrigger()
+                .forJob(jobDetail1)
                 .withIdentity(ID + " 01Trigger")
-                .withSchedule(scheduleBuilder)
+                .withSchedule(jobOneScheduleBuilder)
                 .startNow() //立即執行一次任務
                 .build();
+        Trigger trigger2 = TriggerBuilder.newTrigger()
+                .forJob(jobDetail2)
+                .withIdentity(ID + " 02Trigger")
+                .withSchedule(jobTwoScheduleBuilder)
+                .startNow() //立即執行一次任務
+                .build();
+
         // 手動將觸發器與任務繫結到排程器內
-        scheduler.scheduleJob(jobDetail, trigger);
+        scheduler.scheduleJob(jobDetail1, trigger1);
+        scheduler.scheduleJob(jobDetail2, trigger2);
     }
 }
